27abdeece139f54ba55b7ff8f3c948d9ad285a8c
ThisSeanZhang <thisseanzhang@gmail.com>
2021-12-15 15:27:39 +0000

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ FROM bitnami/nginx:latest
 WORKDIR /app
 COPY dist /app
 COPY ui-nginx.conf /opt/bitnami/nginx/conf/server_blocks
-
+EXPOSE 80
 ENTRYPOINT nginx -g "daemon off;"
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,7 +1,7 @@
 <template>
   <n-layout style="height: 100%;">
-    <n-layout-header style="height: 64px; padding: 24px;" bordered >颐和园路</n-layout-header>
-    <n-layout position="absolute" style="top: 64px; bottom: 64px;" has-sider>
+    <n-layout-header style="height: 64px; padding: 24px;" bordered ></n-layout-header>
+    <n-layout position="absolute" style="top: 64px; bottom: 0px;" has-sider>
       <n-layout-sider
         collapse-mode="transform"
         :collapsed-width="0"
@@ -35,13 +35,12 @@
           <router-view class="contents"/>
       </n-layout-content>
     </n-layout>
-    <n-layout-footer
+    <!-- <n-layout-footer
       position="absolute"
       style="height: 64px; padding: 24px;"
       bordered
     >
-      城府路
-    </n-layout-footer>
+    </n-layout-footer> -->
   </n-layout>
 </template>
 
